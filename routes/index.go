81a16001163e3055cb294b61e2db81cf235351e1
TonyLiu0430 <tony04302020@gmail.com>
2024-11-22 23:40:20 +0800

    style: Change indentation to spaces

--- routes/index.go
+++ routes/index.go
@@ -10,20 +10,20 @@ func AddIndexRoutes(rg *gin.RouterGroup) {
 	rg.GET("", func(c *gin.Context) {
 		c.Data(http.StatusOK, "text/html; charset=utf-8", []byte(
 			`
-			<!DOCTYPE html>
-			<html>
-				<head>
-					<title>Login Demo</title>
-					<button id="login">Login</button>
-				</head>
-			</html>
+            <!DOCTYPE html>
+            <html>
+                <head>
+                    <title>Login Demo</title>
+                    <button id="login">Login</button>
+                </head>
+            </html>
 
-			<script>
+            <script>
 
-				document.getElementById('login').addEventListener('click', async () => {
-					window.location.href = "/api/login";
-				});
-			</script>
-			`))
+                document.getElementById('login').addEventListener('click', async () => {
+                    window.location.href = "/api/login";
+                });
+            </script>
+            `))
 	})
 }
